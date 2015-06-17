be720b34db61254274ecd4429d96838649f986e1
tommydevdocs <tommydevdocs@gmail.com>
2015-06-17 17:34:23 -0500

    Initial Commit

---
+++ HelperMethods/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        'displayTheMessage()
+        'Dim myVal As String = superSecretFormula()
+        'Dim myVal As String = superSecretFormula("Bob")
+        'Console.WriteLine(myVal)
+
+        Console.WriteLine(superSecretFormula())
+        Console.WriteLine(superSecretFormula("Bob"))
+        Console.ReadLine()
+    End Sub
+
+    'Sub displayTheMessage()
+    '    Console.WriteLine("From displayTheMessage")
+    'End Sub
+
+    Function superSecretFormula() As String
+        Return "Hello World!"
+    End Function
+
+    Function superSecretFormula(ByVal name As String)
+        Return String.Format("Hello World, {0}", name)
+    End Function
+
+End Module
